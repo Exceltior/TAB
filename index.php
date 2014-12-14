7c5b3b47aed017de7461ded94b48b3322cee7ad5
ale-php <alexandre@progs.net.br>
2014-12-14 20:39:00 -0200

    base

--- index.php
+++ index.php
@@ -1,160 +1,211 @@
-<!DOCTYPE html>
-<html>
-<head>      
-<title>Download músicas, Filmes, Jogos, Programas, Series e muito mais! The Anony Bay - O maior site de Torrents do Brasil</title>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta http-equiv="content-language" content="pt-br">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta name="description" content="Download musicas, músicas, Filmes, Jogos, Programas, Séries e muito mais!. The Anony Bay - O Maior Site de Torrents do Brasil."/>
-<meta name="keywords" content="mp3, avi, 3gp, mp4, Anonymousbr, bittorrent, torrent, torrents, movies, music, games, applications, apps, download, upload, share, kopimi, compartilhar, filmes, Músicas, Jogos, Programas, Android, Aplicativos,"/>
-<script src="/js/tpb.js" type="text/javascript"></script>
-<script type="text/javascript">
-if (top != self) top.location.href = location.href;
-</script>
+<?php
+date_default_timezone_set('America/Sao_Paulo');
+/*
+ *---------------------------------------------------------------
+ * APPLICATION ENVIRONMENT
+ *---------------------------------------------------------------
+ *
+ * You can load different configurations depending on your
+ * current environment. Setting the environment also influences
+ * things like logging and error reporting.
+ *
+ * This can be set to anything, but default usage is:
+ *
+ *     development
+ *     testing
+ *     production
+ *
+ * NOTE: If you change these, also change the error_reporting() code below
+ *
+ */
+	define('ENVIRONMENT', 'development');
+/*
+ *---------------------------------------------------------------
+ * ERROR REPORTING
+ *---------------------------------------------------------------
+ *
+ * Different environments will require different levels of error reporting.
+ * By default development will show errors but testing and live will hide them.
+ */
 
-<style type="text/css">
-body{
-	text-align: center;
-	font-family:Verdana, Arial, Helvetica, sans-serif;
-	font-size:.7em;
-	margin: 10px;
-	color: #000;
-	background: #fff;
-	min-width: 520px;
-}
-a{
-	color: #009;
-	text-decoration: none;
-	border-bottom: 1px dotted #4040D9;
-}
-a:hover{
-	text-decoration: none;
-	border-bottom: 1px solid #009;
-}
-#searchfield{
-	display: block;
-	width: 500px;
-	margin: 0 auto 20px auto;
-	text-align: center;
-	padding: 0 0 0 100px;
-}
+if (defined('ENVIRONMENT'))
+{
+	switch (ENVIRONMENT)
+	{
+		case 'development':
+			error_reporting(E_ALL);
+		break;
+	
+		case 'testing':
+		case 'production':
+			error_reporting(0);
+		break;
 
-.searchBox{
-	padding-left: 3px;
-	padding-bottom: 3px;
-	padding-top: 3px;
-	background-image:url('/img/icon-https.gif');
-  	background-repeat:no-repeat;
-	background-position: right;
-	padding-right: 18px;
-}
-
-h1 { padding-right: .7em; }
-
-h1 a {
-	margin: 0 auto;
-	width: 275px;
-	height: 295px;
-	text-indent: -9999px;
-	display: block;
-	background: url(/img/doodles/certificate.jpg) no-repeat;
-	border: 0;
-}
-h3 { font-size: 1.2em; }
-h1 a:hover { border: 0; text-decoration: none; }
-.smalltext {
-	font-size:.8em;
+		default:
+			exit('The application environment is not set correctly.');
 	}
-#fbanners { padding: 10px; margin: 0 auto; }
-#fbanners a { border: 0; margin: 0 7px; color: #000; }
-#fbanners img { border: 0; }
-#fbanners #kopimi { display: block; margin: 20px auto 0 auto; width: 40px;}
-.noborder, .noborder:hover, .noborder img { border: 0; text-decoration: 0; }
+}
 
-#topright {
-position: absolute;
-right: 0;
-top: 0;
-display: block;
-height: 204px;
-width: 205px;
-cursor: pointer;
-text-decoration: none;}
-#topright a {border: 0;}
-</style>
+/*
+ *---------------------------------------------------------------
+ * SYSTEM FOLDER NAME
+ *---------------------------------------------------------------
+ *
+ * This variable must contain the name of your "system" folder.
+ * Include the path if the folder is not in the same  directory
+ * as this file.
+ *
+ */
+	$system_path = 'system';
 
-</head
-<body>
-<div id="topright"><a href="http://www.bytelove.com/tpb" target="_blank"><img src="img/blTrial.gif" border="0" alt=""/></a></div>
-<div class="searchfield" style="margin:1em auto 0 auto;">
-    <img width="500" height="300" src="http://ibxk.com.br/materias/3175/9710.jpg?w=1040" />
-<form action="busca/0" class='right' id='searchthis' method='post'>
+/*
+ *---------------------------------------------------------------
+ * APPLICATION FOLDER NAME
+ *---------------------------------------------------------------
+ *
+ * If you want this front controller to use a different "application"
+ * folder then the default one you can set its name here. The folder
+ * can also be renamed or relocated anywhere on your server.  If
+ * you do, use a full server path. For more info please see the user guide:
+ * http://codeigniter.com/user_guide/general/managing_apps.html
+ *
+ * NO TRAILING SLASH!
+ *
+ */
+	$application_folder = 'application';
 
-<table style="margin:0 auto; ">
-	<tr>
-	 <td rowspan="4" width="150">&nbsp;</td>
+/*
+ * --------------------------------------------------------------------
+ * DEFAULT CONTROLLER
+ * --------------------------------------------------------------------
+ *
+ * Normally you will set your default controller in the routes.php file.
+ * You can, however, force a custom routing by hard-coding a
+ * specific controller class/function here.  For most applications, you
+ * WILL NOT set your routing here, but it's an option for those
+ * special instances where you might want to override the standard
+ * routing in a specific front controller that shares a common CI installation.
+ *
+ * IMPORTANT:  If you set the routing here, NO OTHER controller will be
+ * callable. In essence, this preference limits your application to ONE
+ * specific controller.  Leave the function name blank if you need
+ * to call functions dynamically via the URI.
+ *
+ * Un-comment the $routing array below to use this feature
+ *
+ */
+	// The directory name, relative to the "controllers" folder.  Leave blank
+	// if your controller is not in a sub-folder within the "controllers" folder
+	// $routing['directory'] = '';
 
-         <td>
-          <div style="white-space:nowrap;" id="noBreak"><b>Procurar Torrents</b>&nbsp;&nbsp;|&nbsp;<a href="/browse" title="Browse Torrents">Categorias Torrents</a>&nbsp;&nbsp;|&nbsp;<a href="/torrents/0" title="Envios Recentes">Envios Recentes</a>&nbsp;&nbsp;|&nbsp;<a href="/top" title="Top 100">Top 100</a></div>
-         </td>
+	// The controller class file name.  Example:  Mycontroller
+	// $routing['controller'] = '';
 
-         <td>&nbsp;</td>
-      </tr>
-        <tr>
-	 <td style="width:427px;">
-	  <input class="input-xxlarge search-query" type="text" name="busca" id="busca" size="60" />
-	 </td>
-	 <td style="width:150px; text-align:left;" class="smalltext">
-          <a href="/settings" title="Preferences">Preferences</a><br /><a href="/language" title="Languages">Languages</a>
-
-	 </td>
-	</tr>
-
- 	
- </table>
-<input type="hidden" name="page" value="0" />
-<input type="hidden" name="orderby" value="99" />
-</form>
-</div>
-	<h3><a href="/help" title="How do I download?" style="border-bottom: none;">Como faço para baixar?</a></h3>
-
-<div id="foot" style="text-align:center;margin-top:1em;">
-<p>
-<a href="#" title="Login">Logar</a> | <a href="/register" title="Registrar">Registrar</a> | <a href="#" title="Selecionar idioma">Selecionar idioma</a> | 
-<a href="#" title="#">Sobre-nos</a> | 
-
-
-<a href="#" title="#">Blog</a>
-<br/>
-<a href="#" title="#">Contato</a> | 
-<a href="#" title="#">Usage policy</a> | 
-<a href="#" title="#">Downloads</a> | 
-<a href="#" title="#">Doodles</a> | 
-
-<a href="#" title="Forum" target="_blank">Forum</a> | 
-<b><a href="#" title="Facebook" target="_blank">Facebook</a></b>
-<br/>
-<a href="http://slopsbox.com" title="SlopsBox" target="_blank">SlopsBox</a> | 
+	// The controller function you wish to be called.
+	// $routing['function']	= '';
 
 
-<a href="https://github.com/theanonybay" title="PasteBay" target="_blank">Source Code</a> |
- <b><a href="http://ipredator.se" title="IPREDator" target="_blank">IPREDator</a></b>
+/*
+ * -------------------------------------------------------------------
+ *  CUSTOM CONFIG VALUES
+ * -------------------------------------------------------------------
+ *
+ * The $assign_to_config array below will be passed dynamically to the
+ * config class when initialized. This allows you to set custom config
+ * items or override any default config values found in the config.php file.
+ * This can be handy as it permits you to share one application between
+ * multiple front controller files, with each file containing different
+ * config values.
+ *
+ * Un-comment the $assign_to_config array below to use this feature
+ *
+ */
+	// $assign_to_config['name_of_config_item'] = 'value of config item';
 
-<br/>
-</p>
 
-<p id="footer" style="color:#666; font-size:0.9em; ">
-      <!-- Aqui Ficará o registro de usuarios e de arquivos enviados no servidor -->
-        3.731.461 registered users. Last updated 19:28:04.<br />
-        IPv4 19.678.940 peers (9.935.545 seeders + 9.743.395 leechers) in 1.786.657 torrents on tracker.<br />
-        IPv6 287.418 peers (160.464 seeders + 126.954 leechers) in 202.137 torrents on tracker.<br />
-</p>
 
-<div id="open-software" style="width: 600px; color: #666; margin: 20px auto 0 auto;">
-		Nós Adoramos Coisas Grátis.
-		
-	</div>
-</div>
-</body>
-</html>
+// --------------------------------------------------------------------
+// END OF USER CONFIGURABLE SETTINGS.  DO NOT EDIT BELOW THIS LINE
+// --------------------------------------------------------------------
+
+/*
+ * ---------------------------------------------------------------
+ *  Resolve the system path for increased reliability
+ * ---------------------------------------------------------------
+ */
+
+	// Set the current directory correctly for CLI requests
+	if (defined('STDIN'))
+	{
+		chdir(dirname(__FILE__));
+	}
+
+	if (realpath($system_path) !== FALSE)
+	{
+		$system_path = realpath($system_path).'/';
+	}
+
+	// ensure there's a trailing slash
+	$system_path = rtrim($system_path, '/').'/';
+
+	// Is the system path correct?
+	if ( ! is_dir($system_path))
+	{
+		exit("Your system folder path does not appear to be set correctly. Please open the following file and correct this: ".pathinfo(__FILE__, PATHINFO_BASENAME));
+	}
+
+/*
+ * -------------------------------------------------------------------
+ *  Now that we know the path, set the main path constants
+ * -------------------------------------------------------------------
+ */
+	// The name of THIS file
+	define('SELF', pathinfo(__FILE__, PATHINFO_BASENAME));
+
+	// The PHP file extension
+	// this global constant is deprecated.
+	define('EXT', '.php');
+
+	// Path to the system folder
+	define('BASEPATH', str_replace("\\", "/", $system_path));
+
+	// Path to the front controller (this file)
+	define('FCPATH', str_replace(SELF, '', __FILE__));
+
+	// Name of the "system folder"
+	define('SYSDIR', trim(strrchr(trim(BASEPATH, '/'), '/'), '/'));
+
+
+	// The path to the "application" folder
+	if (is_dir($application_folder))
+	{
+		define('APPPATH', $application_folder.'/');
+	}
+	else
+	{
+		if ( ! is_dir(BASEPATH.$application_folder.'/'))
+		{
+			exit("Your application folder path does not appear to be set correctly. Please open the following file and correct this: ".SELF);
+		}
+
+		define('APPPATH', BASEPATH.$application_folder.'/');
+	}
+
+/* --------------------------------------------------------------------
+ * LOAD THE DATAMAPPER BOOTSTRAP FILE
+ * --------------------------------------------------------------------
+ */
+require_once APPPATH.'third_party/datamapper/bootstrap.php';
+
+/*
+ * --------------------------------------------------------------------
+ * LOAD THE BOOTSTRAP FILE
+ * --------------------------------------------------------------------
+ *
+ * And away we go...
+ *
+ */
+require_once BASEPATH.'core/CodeIgniter.php';
+
+/* End of file index.php */
+/* Location: ./index.php */
